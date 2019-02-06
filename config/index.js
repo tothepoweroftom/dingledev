e3cb8da669aca57c1623cb3baf1108abb7920218
tom <powerth@tcd.ie>
2019-02-06 16:48:57 +0100

    add docs

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/dingledev',
+    assetsPublicPath: 'https://tothepoweroftom.github.io/dingledev',
 
     /**
      * Source Maps
